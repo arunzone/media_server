d0b5083094d286786d443e5cf6297dbe916e18c0
Arun <aruncontacts@gmail.com>
2024-09-28 14:50:41 +1000

    Add samba for file share

--- prepare.sh
+++ prepare.sh
@@ -10,6 +10,8 @@ ansible-galaxy collection install community.general
 
 source ~/.bashrc
 
+ansible-galaxy collection install -r requirements.yml
+
 ansible-playbook -vvv -i "localhost," --ask-become-pass \
 --extra-vars "ansible_user=media" \
 --extra-vars "ssh_port_number=$ssh_port_number" \
